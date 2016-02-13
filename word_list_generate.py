27aa75fa96757474c69c95f49a7f5a9c4a219066
Michael Herman <hermanmu@gmail.com>
2016-02-12 18:04:03 -0700

    added python3 support

--- word_list_generate.py
+++ word_list_generate.py
@@ -1,10 +1,10 @@
-import re
 import random
 
 f = open('word_list.txt', 'r')
 wordlist = []
 for line in f:
     wordlist.append(line.strip())
+
 
 def generate_sql(table='testing', column=['word_col', 'int_col', 'int_col2'], col_type=['varchar', 'int', 'int'], line_ending="\n", rows=2048):
     line = ""
@@ -15,13 +15,14 @@ def generate_sql(table='testing', column=['word_col', 'int_col', 'int_col2'], co
             if v == 'varchar':
                 line = line + "'" + random_sentence() + "', "
             elif v == 'int':
-                line = line + "" + str(random.randint(1,10000)) + ", "
-        line = line.rstrip(', ');
+                line = line + "" + str(random.randint(1, 10000)) + ", "
+        line = line.rstrip(', ')
         if i == rows - 1:
             line = line + ");" + line_ending
         else:
             line = line + "), " + line_ending
     return line
+
 
 def random_sentence(i=3):
     global wordlist
@@ -32,7 +33,7 @@ def random_sentence(i=3):
 
 wf = open('sql/insert.sql', 'wb')
 
-print "generating sql"
-wf.write(generate_sql(rows=10000))
+print("generating sql")
+wf.write(generate_sql(rows=10000).encode('utf-8'))
 wf.close()
 f.close
